49d1def2605b69df90607df0c31cab12eed9d6b8
enestorovic <enestorovic@gmail.com>
2015-02-13 16:01:18 -0500

    fixed wrong url

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@23.253.55.188}
+role :web, %w{deploy@23.253.55.188}
+role :db,  %w{deploy@23.253.55.188}
 
 
 # Extended Server Syntax
